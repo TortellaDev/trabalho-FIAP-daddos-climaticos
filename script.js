8f75107eccc06a2b271a336bf11bde3b19616eb4
TortellaDev <tortella.producao@gmail.com>
2025-06-25 09:09:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -37,16 +37,22 @@ async function consultarAr() {
     if (resposta.status === 200) { 
       const dados = await resposta.json(); 
 
-      resultado.innerHTML = ` 
-        <h3>Qualidade do ar em <strong>${cidade}</strong></h3> 
-        <p><strong>Índice Geral (AQI):</strong> ${dados.overall_aqi} — quanto menor, melhor.</p> 
-        <p><strong>Partículas finas (PM2.5):</strong> ${dados["PM2.5"].concentration} µg/m³ 
-        <br><em>Classificação:</em> ${classificarPM25(dados["PM2.5"].concentration)} </p>
-        <p><strong>Dióxido de Nitrogênio (NO2):</strong> ${dados["NO2"].concentration} µg/m³ 
-        <br><em>Classificação:</em> ${classificarNO2(dados["NO2"].concentration)} </p>
-        <p><strong>Ozônio ao nível do solo (O3):</strong> ${dados["O3"].concentration} µg/m³ 
-        <br><em>Classificação:</em> ${classificarO3(dados["O3"].concentration)} </p>
-      `; 
+      resultado.innerHTML = `  
+<h3>Qualidade do ar em <strong>${cidade}</strong></h3>     
+Agora que você consulta dados da qualidade do ar, que tal tornar isso mais útil para quem usa sua aplicação?
+
+Crie regras simples para classificar os níveis de poluentes como:
+Índice
+<p><strong>Índice Geral (AQI):</strong> ${dados.overall_aqi} — quanto menor, melhor.</p>     
+<p><strong>Partículas finas (PM2.5):</strong> ${dados["PM2.5"].concentration} μg/m3   <br><em>Classificação:</em> ${classificarPM25(dados["PM2.5"].concentration)}  
+<br><small>Essas partículas penetram nos pulmões e podem afetar a saúde respiratória.</small></p>  
+  
+<p><strong>Dióxido de Nitrogênio (NO2):</strong> ${dados["NO2"].concentration} μg/m3   <br><em>Classificação:</em> ${classificarNO2(dados["NO2"].concentration)}  
+<br><small>Gás comum em áreas urbanas com tráfego intenso. Pode causar irritações.</small></p>  
+  
+<p><strong>Ozônio ao nível do solo (O3):</strong> ${dados["O3"].concentration} μg/m3   <br><em>Classificação:</em> ${classificarO3(dados["O3"].concentration)}  
+<br><small>Em excesso, pode causar desconforto respiratório e afetar grupos sensíveis.</small></p>  
+`;  
     } else { 
       resultado.innerHTML = "Cidade não encontrada ou erro na consulta."; 
     } 
